98f43256df39824391d9298fecb8a827a74e98a1
codnjs3575 <codnjs3575@naver.com>
2024-03-05 19:43:00 +0900

    created gitignore & update FrontEnd_메인화면

--- FrontEnd/App.js
+++ FrontEnd/App.js
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>JWTProject</Text>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  text: {
+    color: 'white',
+    fontSize: '19px',
+    fontWeight: 'bold',
+  },
+})
