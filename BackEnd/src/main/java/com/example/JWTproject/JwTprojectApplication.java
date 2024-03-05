f5906bf9953c977598db8c7166edd6b04c5c93c9
TaehyunJung <123145738+TaehyunJung@users.noreply.github.com>
2024-03-05 19:30:14 +0900

    backend_spring

---
+++ BackEnd/src/main/java/com/example/JWTproject/JwTprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.JWTproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwTprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwTprojectApplication.class, args);
+	}
+
+}
